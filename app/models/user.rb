d82755586e87729f89d889caff679f6bc4e3eba3
lflores1961 <fernando.flores1961@gmail.com>
2021-03-15 16:01:53 -0600

    Add user access level to manage access to different app functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
   before_save { email.downcase! }
-  # enum access_level: { user: 0, admin: 1, owner: 2 }
+  enum access_level: { user: 0, admin: 1, owner: 2 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 250 }, 
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
                     # Beware: this uniqueness code IS NOT ENOUGH
-                    #  Need to add Database index, migration
+                    #  Need to add Database index, migration DONE
 
   validates :password, length: { minimum: 8 }
   has_secure_password
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_14_015620) do
+ActiveRecord::Schema.define(version: 2021_03_15_023142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_03_14_015620) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.integer "access_level", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
