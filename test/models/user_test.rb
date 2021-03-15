641aaa7d775713a47d74530e066f0cb9f06321b3
lflores1961 <fernando.flores1961@gmail.com>
2021-03-14 19:46:00 -0600

    Add User validations and secure password with bcrypt

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,67 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example User", email: "user23@example.com", password: "password", password_confirmation: "password")  
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "Name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "   "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 250 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "Email should accept valid addresses" do
+    valid_addresses = %w[user@example.com user_one@foo.org fooBar@baz.org first.last@some.com alice+bob@mail.com]
+    valid_addresses.each do |valid|
+      @user.email = valid
+      assert @user.valid?, "#{valid.inspect} should be valid"
+    end
+  end
+
+  test "email should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses must be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+  test "Email should be saved as lowercase" do
+    mixed_case_email = "Foo@ExAMPle.com"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
 end
