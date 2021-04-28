dc31f6360125fd0ff481d55db14833f05dc47f60
lflores1961 <fernando.flores1961@gmail.com>
2021-04-28 15:09:27 -0500

    Add user maintenance module with views and styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,23 +5,30 @@ class ApplicationController < ActionController::Base
         [
           {:opcion => "Entradas", :ruta => '#'}, 
           {:opcion => "Salidas", :ruta => '#'}
-        ]
+        ],
+        :macro_ruta => '#'
       },
       {:letrero => "Proveedores", :submenu =>
         [
           {:opcion => "Indice", :ruta => 'suppliers_path'}
-        ]
+        ],
+        :macro_ruta => '#'
       },
       {:letrero => "Productos", :submenu =>
         [
           {:opcion => "Index", :ruta => 'products_path'}, 
           {:opcion => "Autorización", :ruta => '#'}
-        ]
+        ],
+        :macro_ruta => '#'
       },
       {:letrero => "Usuarios", :submenu =>
         [
           {:opcion => "Indice", :ruta => 'users_url'}
-        ]
+        ],
+        :macro_ruta => '#'
+      },
+      {
+        :letrero => "Inicio", :submenu => [], :macro_ruta => 'root_url'
       }
     ]
   end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery"
 import "packs/static_pages"
+import "packs/styles.scss"
 
 Rails.start()
 Turbolinks.start()
