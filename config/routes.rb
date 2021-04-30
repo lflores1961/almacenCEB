63c76e1b8d7a8ac2e86652c4e8de313034881696
lflores1961 <fernando.flores1961@gmail.com>
2021-04-29 20:27:33 -0500

    Add Login and Logout mechanics to Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get     'login'  => 'sessions#new'
+  post    'login'  => 'sessions#create'
+  delete  'logout' => 'sessions#destroy'
+  root    'static_pages#main'
+
   resources :suppliers
   resources :products
-  root 'static_pages#main'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
